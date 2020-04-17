179dc183a99d8c1dd4709939cb7366ff32b244c9
Matt Shine <matt@shinemail.net>
2020-04-17 17:05:14 -0400

    Adding deployment steps to travis build

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          wing
+          Hello Scott Paige.
         </p>
         <a
           className="App-link"
